46a7efd782fa7e0a6664894beb6b3ad1ed581110
savayer <sava0094@gmail.com>
2021-03-26 15:57:52 +0200

    (add) added mobile menu. added gallery data

--- src/blocks/modules/gallery/gallery.js
+++ src/blocks/modules/gallery/gallery.js
@@ -13,27 +13,6 @@ new Vue({
                     'https://photocentra.ru/images/main53/532897_main.jpg',
                     'https://www.soleans.ru/wp-content/uploads/Lagoa-do-Fogo-Sao-Migel.jpg',
                     'http://i.mycdn.me/i?r=AzEPZsRbOZEKgBhR0XGMT1RkgKtXzPh54BUM6z80btI0C6aKTM5SRkZCeTgDn6uOyic',
-                    'https://photocentra.ru/images/main53/532897_main.jpg',
-                    'https://www.soleans.ru/wp-content/uploads/Lagoa-do-Fogo-Sao-Migel.jpg',
-                    'http://i.mycdn.me/i?r=AzEPZsRbOZEKgBhR0XGMT1RkgKtXzPh54BUM6z80btI0C6aKTM5SRkZCeTgDn6uOyic',
-                    'https://photocentra.ru/images/main53/532897_main.jpg',
-                    'https://www.soleans.ru/wp-content/uploads/Lagoa-do-Fogo-Sao-Migel.jpg',
-                    'http://i.mycdn.me/i?r=AzEPZsRbOZEKgBhR0XGMT1RkgKtXzPh54BUM6z80btI0C6aKTM5SRkZCeTgDn6uOyic',
-                    'https://photocentra.ru/images/main53/532897_main.jpg',
-                    'https://www.soleans.ru/wp-content/uploads/Lagoa-do-Fogo-Sao-Migel.jpg',
-                    'http://i.mycdn.me/i?r=AzEPZsRbOZEKgBhR0XGMT1RkgKtXzPh54BUM6z80btI0C6aKTM5SRkZCeTgDn6uOyic',
-                    'https://photocentra.ru/images/main53/532897_main.jpg',
-                    'https://www.soleans.ru/wp-content/uploads/Lagoa-do-Fogo-Sao-Migel.jpg',
-                    'http://i.mycdn.me/i?r=AzEPZsRbOZEKgBhR0XGMT1RkgKtXzPh54BUM6z80btI0C6aKTM5SRkZCeTgDn6uOyic',
-                    'https://photocentra.ru/images/main53/532897_main.jpg',
-                    'https://www.soleans.ru/wp-content/uploads/Lagoa-do-Fogo-Sao-Migel.jpg',
-                    'http://i.mycdn.me/i?r=AzEPZsRbOZEKgBhR0XGMT1RkgKtXzPh54BUM6z80btI0C6aKTM5SRkZCeTgDn6uOyic',
-                    'https://photocentra.ru/images/main53/532897_main.jpg',
-                    'https://www.soleans.ru/wp-content/uploads/Lagoa-do-Fogo-Sao-Migel.jpg',
-                    'http://i.mycdn.me/i?r=AzEPZsRbOZEKgBhR0XGMT1RkgKtXzPh54BUM6z80btI0C6aKTM5SRkZCeTgDn6uOyic',
-                    'https://photocentra.ru/images/main53/532897_main.jpg',
-                    'https://www.soleans.ru/wp-content/uploads/Lagoa-do-Fogo-Sao-Migel.jpg',
-                    'http://i.mycdn.me/i?r=AzEPZsRbOZEKgBhR0XGMT1RkgKtXzPh54BUM6z80btI0C6aKTM5SRkZCeTgDn6uOyic',
                 ]
             },
             {
@@ -45,6 +24,105 @@ new Vue({
                     'https://mirplaneta.ru/images/5/920.jpg',
                     'https://avatars.mds.yandex.net/get-zen_doc/1900011/pub_5fbfdefdc9a19d0e1c29987b_5fbfe278a9416d5dd7f6a0fb/scale_1200',
                     'http://www.vernadsky.ru/images/News2021/02_2020/peregrine_falcon.jpg'
+                ]
+            },
+            {
+                cover: 'img/gallery-covers/3.jpg',
+                title: 'Encore + AfterHalloween',
+                description: '',
+                className: '',
+                photos: [
+                    'https://photocentra.ru/images/main53/532897_main.jpg',
+                    'https://www.soleans.ru/wp-content/uploads/Lagoa-do-Fogo-Sao-Migel.jpg',
+                    'http://i.mycdn.me/i?r=AzEPZsRbOZEKgBhR0XGMT1RkgKtXzPh54BUM6z80btI0C6aKTM5SRkZCeTgDn6uOyic',
+                ]
+            },
+            {
+                cover: 'img/gallery-covers/4.jpg',
+                title: 'Test test',
+                description: 'some description',
+                className: '',
+                photos: [
+                    'https://mirplaneta.ru/images/5/920.jpg',
+                    'https://avatars.mds.yandex.net/get-zen_doc/1900011/pub_5fbfdefdc9a19d0e1c29987b_5fbfe278a9416d5dd7f6a0fb/scale_1200',
+                    'http://www.vernadsky.ru/images/News2021/02_2020/peregrine_falcon.jpg'
+                ]
+            },
+            {
+                cover: 'img/gallery-covers/5.jpg',
+                title: 'Encore + AfterHalloween',
+                description: '',
+                className: '',
+                photos: [
+                    'https://photocentra.ru/images/main53/532897_main.jpg',
+                    'https://www.soleans.ru/wp-content/uploads/Lagoa-do-Fogo-Sao-Migel.jpg',
+                    'http://i.mycdn.me/i?r=AzEPZsRbOZEKgBhR0XGMT1RkgKtXzPh54BUM6z80btI0C6aKTM5SRkZCeTgDn6uOyic',
+                ]
+            },
+            {
+                cover: 'img/gallery-covers/6.jpg',
+                title: 'Test test',
+                description: 'some description',
+                className: '',
+                photos: [
+                    'https://mirplaneta.ru/images/5/920.jpg',
+                    'https://avatars.mds.yandex.net/get-zen_doc/1900011/pub_5fbfdefdc9a19d0e1c29987b_5fbfe278a9416d5dd7f6a0fb/scale_1200',
+                    'http://www.vernadsky.ru/images/News2021/02_2020/peregrine_falcon.jpg'
+                ]
+            },
+            {
+                cover: 'img/gallery-covers/7.jpg',
+                title: 'Encore + AfterHalloween',
+                description: '',
+                className: '',
+                photos: [
+                    'https://photocentra.ru/images/main53/532897_main.jpg',
+                    'https://www.soleans.ru/wp-content/uploads/Lagoa-do-Fogo-Sao-Migel.jpg',
+                    'http://i.mycdn.me/i?r=AzEPZsRbOZEKgBhR0XGMT1RkgKtXzPh54BUM6z80btI0C6aKTM5SRkZCeTgDn6uOyic',
+                ]
+            },
+            {
+                cover: 'img/gallery-covers/8.jpg',
+                title: 'Test test',
+                description: 'some description',
+                className: 'col2-row1',
+                photos: [
+                    'https://mirplaneta.ru/images/5/920.jpg',
+                    'https://avatars.mds.yandex.net/get-zen_doc/1900011/pub_5fbfdefdc9a19d0e1c29987b_5fbfe278a9416d5dd7f6a0fb/scale_1200',
+                    'http://www.vernadsky.ru/images/News2021/02_2020/peregrine_falcon.jpg'
+                ]
+            },
+            {
+                cover: 'img/gallery-covers/9.jpg',
+                title: 'Encore + AfterHalloween',
+                description: '',
+                className: 'col1-row2',
+                photos: [
+                    'https://photocentra.ru/images/main53/532897_main.jpg',
+                    'https://www.soleans.ru/wp-content/uploads/Lagoa-do-Fogo-Sao-Migel.jpg',
+                    'http://i.mycdn.me/i?r=AzEPZsRbOZEKgBhR0XGMT1RkgKtXzPh54BUM6z80btI0C6aKTM5SRkZCeTgDn6uOyic',
+                ]
+            },
+            {
+                cover: 'img/gallery-covers/10.jpg',
+                title: 'Test test',
+                description: 'some description',
+                className: 'col2-row1',
+                photos: [
+                    'https://mirplaneta.ru/images/5/920.jpg',
+                    'https://avatars.mds.yandex.net/get-zen_doc/1900011/pub_5fbfdefdc9a19d0e1c29987b_5fbfe278a9416d5dd7f6a0fb/scale_1200',
+                    'http://www.vernadsky.ru/images/News2021/02_2020/peregrine_falcon.jpg'
+                ]
+            },
+            {
+                cover: 'img/gallery-covers/11.jpg',
+                title: 'Encore + AfterHalloween',
+                description: '',
+                className: '',
+                photos: [
+                    'https://photocentra.ru/images/main53/532897_main.jpg',
+                    'https://www.soleans.ru/wp-content/uploads/Lagoa-do-Fogo-Sao-Migel.jpg',
+                    'http://i.mycdn.me/i?r=AzEPZsRbOZEKgBhR0XGMT1RkgKtXzPh54BUM6z80btI0C6aKTM5SRkZCeTgDn6uOyic',
                 ]
             }
         ]
--- src/blocks/modules/nav/nav.js
+++ src/blocks/modules/nav/nav.js
@@ -8,4 +8,20 @@ $(document).on('click', '.menu a[href^="#"]', function (e) {
 
 $('.nav__burger').click(function () {
     $(this).toggleClass('active')
+    $('.nav__mobile_wrapper').toggleClass('active')
+    if ($(this).hasClass('active')) {
+        $('body').css({'overflow': 'hidden'})
+    } else {
+        $('body').css({'overflow': ''})
+    }
+})
+
+$('.nav__mobile_wrapper').click(function () {
+    $(this).removeClass('active')
+    $('.nav__burger').removeClass('active')
+    if ($(this).hasClass('active')) {
+        $('body').css({'overflow': 'hidden'})
+    } else {
+        $('body').css({'overflow': ''})
+    }
 })
